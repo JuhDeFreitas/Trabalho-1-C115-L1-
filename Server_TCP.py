e44ca2730ff1366011f9a6dfc6aff29786dc21d0
JuhDeFreitas <96407638+JuhDeFreitas@users.noreply.github.com>
2025-03-19 14:33:23 -0300

    Update Server_TCP.py
    Versão 2

--- Server_TCP.py
+++ Server_TCP.py
@@ -1,68 +1,45 @@
 import socket
+import json
 
-HOST = '127.0.0.1'  # Endereço do servidor (localhost)
-PORT = 65432        # Porta do servidor
+HOST = 'localhost'
+PORT = 3000
 
 question1 = "1️⃣O que faz a função socket.bind() em um servidor TCP?\na) Conecta o servidor a um cliente automaticamente.\nb) Define o endereço e a porta onde o servidor vai escutar conexões.\nc) Envia uma mensagem para um cliente conectado.\nd) Fecha o socket do servidor."
 question2 = "2️⃣ Qual é a principal diferença entre TCP e UDP?\na) TCP é orientado a conexão e garante entrega, enquanto UDP é mais rápido, mas sem garantia de entrega.\nb) UDP sempre é mais seguro que TCP.\nc) TCP só pode ser usado para páginas web e UDP só para jogos.\nd) UDP não funciona em redes locais."
 question3 = "3️⃣ O que significa a sigla API em tecnologia?\na) Aplicativo Para Internet\nb) Área de Programação Integrada\nc) Interface de Programação de Aplicações\nd) Arquitetura de Processamento Inteligente"
 answer = ['b', 'a', 'c']
+number_of_questions = 3
 
 # Criando o socket TCP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
-server_socket.listen(1)
+server_socket.bind((HOST, PORT))    #atribui um endereço/porta ao servidor
+server_socket.listen(1)             #conecta a apenas um cliente por vez
+print(f"Aguardando conexão na porta {PORT}...")
 
-print(f"Servidor aguardando conexão na porta {PORT}...")
+# Aceita conexão com o Client
+connection_socket, addr_client = server_socket.accept()
+print(f"Conexão estabelecida.\nCliente: {addr_client}")
 
-# Aceita a conexão do cliente
-connectionSocket, addr = server_socket.accept()
-print(f"Conectado a {addr}")
+# Envio das questões
+connection_socket.send(str(number_of_questions).encode())
+connection_socket.send(question1.encode())
+connection_socket.send(question2.encode())
+connection_socket.send(question3.encode())
+print("Questões enviadas ao cliente.")
 
-# Envia as perguntas ao cliente
-connectionSocket.sendall(question1.encode())
-connectionSocket.sendall(question2.encode())
-connectionSocket.sendall(question3.encode())
-print("Questões enviadas ao cliente")
+# Recebe as respostas do Client
+response = list(connection_socket.recv(1024).decode())
+print(f"Respostas recebidas.")
 
-# Recebe a resposta do cliente
-response1 = connectionSocket.recv(1024).decode()
-response2 = connectionSocket.recv(1024).decode()
-response3 = connectionSocket.recv(1024).decode()
-print(f"Respostas: {response1, response2, response3}")
+# Verifica as respostas
+status = [False] * len(response)
 
-acertos = 0
-erros = 0
+for i in range(number_of_questions):
+    if response[i] == answer[i]:
+        status[i] = True
+    else:
+        status[i] = False
 
-# Realizando a correção das questões
-if response1 == answer[0]:
-    result1 = "Questão 1: Correta"
-    acertos += 1
-else:
-    result1 = "Questão 1: Incorreta"
-    erros += 1
-    
-if response2 == answer[1]:
-    result2 = "Questão 2: Correta"
-    acertos += 1
-else:
-    result2 = "Questão 2: Incorreta"
-    erros += 1
-    
-if response3 == answer[2]:
-    result3 = "Questão 3: Correta"
-    acertos += 1
-else:
-    result3 = "Questão 3: Incorreta"
-    erros += 1
-   
-# Retornando a correção
-connectionSocket.sendall(result1.encode())
-connectionSocket.sendall(result2.encode())
-connectionSocket.sendall(result3.encode())
-connectionSocket.sendall(acertos.encode())
-connectionSocket.sendall(erros.encode())
-
-# Fecha a conexão
-connectionSocket.close()
-server_socket.close()
+# Envia o resultado do questionário ao Client (Formato JSON)
+connection_socket.send(json.dumps(status).encode())
+print("Resultado do questionário enviado.")
